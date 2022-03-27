40417c1ae25ca46cbf36783b017d7270219419f9
sidney.silva <sidney.silva@sepog.fortaleza.ce.gov.br>
2022-03-27 15:03:51 -0300

    Primer Commit

---
+++ app.js
@@ -0,0 +1,13 @@
+
+const {crearArchivo} = require('./helpers/multiplicar');
+const argv = require('./config/yargs');
+require('colors');
+
+
+console.clear();
+
+console.log(argv);
+
+crearArchivo(argv.b, argv.l,argv.h)
+.then(nombreArchivo => console.log(nombreArchivo.rainbow, 'creado'))
+.catch(err => console.log(err));
